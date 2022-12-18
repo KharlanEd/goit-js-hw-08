2764559857af630e31c374b6d5abeec6de6aa937
KharlanEd <eduardxarlan@gmail.com>
2022-12-18 11:10:15 +0200

    final

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+ const containerGallery = document.querySelector('.gallery');
+
+const cardMarcup = createCardMarcup(galleryItems);
+
+containerGallery.insertAdjacentHTML('beforeend',cardMarcup)
+
+    function createCardMarcup(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+}).join('');
+};
+
+ 
+   new SimpleLightbox('.gallery a', {
+        captionsData: `alt`,
+        captionDelay: 250
+    })
+
+console.log(galleryItems)
